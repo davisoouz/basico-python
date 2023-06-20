a53955d3e4c020888ba72bc6af7cd2b72b751833
Davi Emanuel <136705446+davisoouz@users.noreply.github.com>
2023-06-20 08:34:34 -0300

    TipoVariavel.py

--- TipoVariavel.py
+++ TipoVariavel.py
@@ -3,6 +3,6 @@ salario = 1500.00
 nome = 'Jose'
 situação = True
 
-tipo = type (salario) #Específica o tipo da variável
+tipo = type (salario) #Específica o tipo da variável, no caso aqui, é do tipo float
 
 print("Código é: ", codigo, "o salário é ", salario, "o nome é ", nome, "e o tipo de salário é ", tipo)
